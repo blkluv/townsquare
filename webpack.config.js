e3169fce7d90fe27780a58449ee2a8d1d30d0510
Zayyan Faizal <zayyan@usedispatch.xyz>
2023-08-30 10:53:22 -0500

    revert linter changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
-const createExpoWebpackConfigAsync = require("@expo/webpack-config");
-const { withAlias } = require("@expo/webpack-config/addons");
-const { buffer } = require("stream/consumers");
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const { withAlias } = require('@expo/webpack-config/addons');
 
 const reactNativeWebWebviewConfiguration = {
   test: /postMock.html$/,
   use: {
-    loader: "file-loader",
+    loader: 'file-loader',
     options: {
-      name: "[name].[ext]",
+      name: '[name].[ext]',
     },
   },
 };
@@ -15,22 +14,21 @@ const reactNativeWebWebviewConfiguration = {
 module.exports = async function (env, argv) {
   let config = await createExpoWebpackConfigAsync(env, argv);
   config = withAlias(config, {
-    "react-native$": "react-native-web",
-    "react-native-linear-gradient": "react-native-web-linear-gradient",
-    "react-native-webview": "react-native-web-webview",
+    'react-native$': 'react-native-web',
+    'react-native-linear-gradient': 'react-native-web-linear-gradient',
+    'react-native-webview': 'react-native-web-webview',
   });
   config.module.rules = [
     ...(config.module.rules || []),
     reactNativeWebWebviewConfiguration,
   ];
   config.resolve.fallback = {
-    "http": false,
-    "zlib": false,
-    "url": false,
-    "stream": false,
-    "https": false,
-    // "safe-buffer": false,
-    "crypto": false,
+    'http': false,
+    'zlib': false,
+    'url': false,
+    'stream': false,
+    'https': false,
+    'crypto': false,
   };
   return config;
 };
